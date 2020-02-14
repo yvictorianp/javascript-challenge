a96135f7468e41ccb17a207d8e2cae5ef751a501
yvictorianp <56240830+yvictorianp@users.noreply.github.com>
2020-02-13 19:38:48 -0500

    final

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,48 +1,53 @@
 // from data.js
 var tableData = data;
 
-function tableDisplay(ufoSightings) {
-    var tbody = d3.select("tbody");
-    ufoSightings.forEach((ufoRecord) => {
-      var row = tbody.append("tr");
-      Object.entries(ufoRecord).forEach(([key, value]) => {
-        var cell = row.append("td");
-        cell.html(value);
-      });
+// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+console.log(data);
+
+data.forEach(function(ufoSightings) {
+    console.log(ufoSightings);
+});
+
+data.forEach(function(ufoSightings) {
+    console.log(ufoSightings);
+    var row = tbody.append("tr");
+});
+
+data.forEach(function(ufoSightings) {
+    console.log(ufoSightings);
+    var row = tbody.append("tr");
+
+    Object.entries(ufoSightings).forEach(function([key, value]) {
+        console.log(key, value);
     });
-  };
-  
-  function deleteTbody() {
-    d3.select("tbody")
-      .selectAll("tr").remove()
-      .selectAll("td").remove();
-  };
-  
-  console.log(tableData);
-  tableDisplay(tableData);
-  
-  var button = d3.select("#filter-btn");
-  
-  button.on("click", function(event) {
-    d3.event.preventDefault();
-    deleteTbody();
-    var dateInput = d3.select("#datetime").property("value");
-    
-    if (dateInput.trim() === "" ) {
-      var filteredData = tableData;
-    } else {  
-      var filteredData = tableData.filter(ufoSighting => 
-        ufoSighting.datetime === dateInput.trim());
-    };
-  
-    if (filteredData.length == 0) {
-      d3.select("tbody")
-        .append("tr")
-        .append("td")
-          .attr("colspan", 7)
-          .html("<h4>No Records Found</h4>");
-    };
-  
-    console.log(filteredData);
-    tableDisplay(filteredData);
+});
+
+data.forEach(function(ufoSightings) {
+    console.log(ufoSightings);
+    var row = tbody.append("tr");
+
+    Object.entries(ufoSightings).forEach(function([key, value]) {
+        console.log(key, value);
+        var cell = tbody.append("td");
+    });
+});
+
+data.forEach(function(ufoSightings) {
+    console.log(ufoSightings);
+    var row = tbody.append("tr");
+    Object.entries(ufoSightings).forEach(function([key, value]) {
+        console.log(key, value);
+        var cell = tbody.append("td");
+        cell.text(value);
+    });
+});
+
+data.forEach((ufoSightings) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoSightings).forEach(([key, value]) => {
+      var cell = tbody.append("td");
+      cell.text(value);
+    });
   });
